a3b869287159193a0f008672d42d37f9fc7aae9c
Vivian Fan <88053079+vivian-fan@users.noreply.github.com>
2022-02-04 13:53:33 -0800

    Returns json as string

--- .github/bump-version.py
+++ .github/bump-version.py
@@ -3,6 +3,7 @@ import sys
 import shutil
 import yaml
 import git
+import json
 from json.decoder import JSONDecodeError
 
 def get_intents():
@@ -86,4 +87,4 @@ for file_name, intent in intents['intent'].items():
 shutil.rmtree(release_path)
 shutil.rmtree(target_path)
 
-print(intent_list)
+print(json.dump(intent_list))
