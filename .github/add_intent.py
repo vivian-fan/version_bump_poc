ddb7f5822b8033cd950bfedb91d8c0a128952ccc
Vivian Fan <88053079+vivian-fan@users.noreply.github.com>
2022-02-17 14:31:01 -0800

    Update add_intent.py

--- .github/add_intent.py
+++ .github/add_intent.py
@@ -22,8 +22,9 @@ def get_clone_repo(remote, path, branch):
 def get_intents(path):
     with open(path + "/.github/intent.yml", "r") as intent_mgmt_file:
         intent_mgmt_content = yaml.safe_load(intent_mgmt_file)
+    if len(intent_mgmt_content) == 0:
+        intent_mgmt_content['intent'] = []
     return intent_mgmt_content
-
 
 def get_intent(path):
     with open(path + "/intent.yml", "r") as intent_file:
