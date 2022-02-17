0bfe50ad03df82f5754ad814f295882846054a2e
Vivian Fan <88053079+vivian-fan@users.noreply.github.com>
2022-02-17 14:31:47 -0800

    Update manage_intent.py

--- .github/manage_intent.py
+++ .github/manage_intent.py
@@ -25,6 +25,8 @@ def get_clone_repo(remote, path, branch):
 def get_intents(path):
     with open(path + "/.github/intent.yml", "r") as intent_mgmt_file:
         intent_mgmt_content = yaml.safe_load(intent_mgmt_file)
+    if len(intent_mgmt_content) == 0:
+        intent_mgmt_content['intent'] = []
     return intent_mgmt_content
 
 
@@ -48,27 +50,14 @@ def push_to_origin(intent_mgmt_content, target_path, target_branch):
 
 
 remote = get_remote()
-# Clone master branch
-# Read intent_list from /.github/intent.yml as master_intents
-# master_intents = ???
-# delete the intent in the released_intents from master_intents
-# Push back to master
 master_path = "./master"
 clone_repo_master = get_clone_repo(remote, master_path, "master")
 master_intents = get_intents(master_path)
 
-# Clone develop branch
-# Read intent_list from /.github/intent.yml as dev_intents
-# dev_intents = ???
-# delete the intent in the released_intents from dev_intents
-# Push back to develop
 dev_path = "./develop"
 clone_repo_master = get_clone_repo(remote, dev_path, "develop")
 dev_intents = get_intents(dev_path)
 
-# Clone latest_release branch
-# Read intent_list from /.github/intent.yml as released_intents
-# released_intents = ???
 release_path = "./release"
 release_branches = []
 for branch in clone_repo_master.refs:
